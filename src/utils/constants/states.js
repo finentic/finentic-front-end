f4d6e9ac497007b04eaaddc270f4535bce150e18
luc.letuan <phantoinang2001@gmail.com>
2023-05-04 15:44:46 +0700

    Update several constants

--- src/utils/constants/states.js
+++ src/utils/constants/states.js
@@ -12,7 +12,17 @@ const BUTTON_STATE = {
     REJECTED: 'REJECTED'
 }
 
+const ITEM_STATE = {
+    CREATED: 'CREATED',
+    LISTING: 'LISTING',
+    SOLD: 'SOLD',
+    DELIVERED: 'DELIVERED',
+    CANCELED: 'CANCELED',
+    HIDDEN: 'HIDDEN',
+}
+
 export {
     BUTTON_STATE,
     ACCOUNT_STATE,
+    ITEM_STATE,
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,14 @@
 import { BigNumber } from "ethers"
+import { API_BASE_URI } from "./constants"
 
 const toBN = (value) => BigNumber.from(value.toString())
 const getBlockTimestamp = () => Math.floor((new Date()).getTime() / 1000.0)
+const toImgUrl = (url) => `${API_BASE_URI}/${url}`
+const toRawUrl = (itemId) => `${API_BASE_URI}/items/raw/${itemId}`
 
 export {
     toBN,
     getBlockTimestamp,
+    toImgUrl,
+    toRawUrl,
 }
