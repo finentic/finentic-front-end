7b9f16dcf536da5401f8d7705347dfb259926c9a
luc.letuan <phantoinang2001@gmail.com>
2023-05-11 14:57:34 +0700

    Optional props

--- src/components/button-img/ButtonImg.jsx
+++ src/components/button-img/ButtonImg.jsx
@@ -1,37 +1,40 @@
 import { Button, Image, OverlayTrigger, Tooltip } from 'react-bootstrap'
 
 
-function ButtonImg({ imgUrl, title, tooltip, onClick, className }) {
-    return (
-      <OverlayTrigger
-        placement="bottom"
-        overlay={
+function ButtonImg({ imgUrl, title, tooltip, onClick, className, size = 28, fontSize = 14 }) {
+  return (
+    <OverlayTrigger
+      placement="bottom"
+      overlay={
+        tooltip
+          ?
           <Tooltip id={title}>
             {tooltip}
           </Tooltip>
-        }
-      >
-        {({ ref, ...triggerHandler }) => (
-          <Button
-            variant="light"
-            {...triggerHandler}
-            className={`d-inline-flex align-items-center py-0 ps-0 border-0 shadow-hover background-color-none-hover ${className}`}
-            style={{
-              borderRadius: '16px',
-            }}
-            onClick={onClick}
-          >
-            <Image
-              ref={ref}
-              roundedCircle
-              src={imgUrl}
-              height='32px'
-            />
-            <span className="ms-1">{title}</span>
-          </Button>
-        )}
-      </OverlayTrigger>
-    );
-  }
+          : <></>
+      }
+    >
+      {({ ref, ...triggerHandler }) => (
+        <Button
+          variant="light"
+          {...triggerHandler}
+          className={`d-inline-flex align-items-center py-0 ps-0 border-0 shadow-hover background-color-none-hover ${className}`}
+          style={{
+            borderRadius: (size / 2),
+          }}
+          onClick={onClick}
+        >
+          <Image
+            ref={ref}
+            roundedCircle
+            src={imgUrl}
+            height={size}
+          />
+          <span className="ms-1 fw-bold text-dark" style={{ fontSize: fontSize }}>{title}</span>
+        </Button>
+      )}
+    </OverlayTrigger>
+  );
+}
 
 export { ButtonImg }
