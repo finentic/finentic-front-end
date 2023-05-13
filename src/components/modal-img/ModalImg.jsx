d2033d50e25fa92fca21f6b08bdbcb2ab8ac905c
luc.letuan <phantoinang2001@gmail.com>
2023-05-14 02:15:45 +0700

    Update modal img

--- src/components/modal-img/ModalImg.jsx
+++ src/components/modal-img/ModalImg.jsx
@@ -1,30 +1,46 @@
+import { faUpRightFromSquare, faX } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Modal } from 'react-bootstrap'
 
-function ModalImg({ show, onHide, imgSrc, name }) {
+function ModalImg({ show, onHide, imgSrc, name, onOpenNewTab }) {
     return (
         <Modal
             show={show}
             onHide={onHide}
-            size="xl"
+            size="lg"
             aria-labelledby="contained-modal-title-vcenter"
             centered
             style={{
                 backdropFilter: "brightness(80%)",
             }}
         >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter" className='fs-6'>
+            <Modal.Header className='pt-1 pb-0 ps-3 pe-1 fs-5'>
+                <Modal.Title id="contained-modal-title-vcenter">
                     {name}
                 </Modal.Title>
+                <div className='float-right'>
+                    {onOpenNewTab && <FontAwesomeIcon
+                        icon={faUpRightFromSquare}
+                        onClick={onOpenNewTab}
+                        width={35}
+                        className='me-2 shadow-hover p-2'
+                    />}
+                    <FontAwesomeIcon
+                        icon={faX}
+                        onClick={onHide}
+                        width={35}
+                        className='shadow-hover p-2'
+                    />
+                </div>
             </Modal.Header>
             <Modal.Body className='p-0'>
                 <img
                     src={imgSrc}
                     alt={name}
                     className="h-100 w-100"
-                    style={{ 
+                    style={{
                         borderRadius: '0px 0px var(--bs-border-radius) var(--bs-border-radius)',
-                     }}
+                    }}
                 />
             </Modal.Body>
         </Modal>
