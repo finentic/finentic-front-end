eecee5aea65e1591b3d6d344ac23f08c14d25d2e
luc.letuan <phantoinang2001@gmail.com>
2023-05-14 02:17:32 +0700

    Update utils

--- src/utils/constants/configurations.js
+++ src/utils/constants/configurations.js
@@ -3,8 +3,9 @@ const PERCENTAGE = '10000'
 
 const DEFAULT_CHAIN = process.env.REACT_APP_DEFAULT_CHAIN
 
-const API_BASE_URI= process.env.REACT_APP_API_BASE_URI
-const RPC_URI= process.env.REACT_APP_RPC_URI
+const API_BASE_URI = process.env.REACT_APP_API_BASE_URI
+const RPC_URI = process.env.REACT_APP_RPC_URI
+const BLOCK_EXPLORER_URL = process.env.REACT_APP_BLOCK_EXPLORER_URL
 
 const FAUCET_URI = {
     AVAX: 'https://faucet.avax.network/'
@@ -17,4 +18,5 @@ export {
     API_BASE_URI,
     RPC_URI,
     FAUCET_URI,
+    BLOCK_EXPLORER_URL,
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,21 +1,38 @@
 import { BigNumber } from "ethers"
-import { API_BASE_URI } from "./constants"
+import { API_BASE_URI, BLOCK_EXPLORER_URL } from "./constants"
 import { commify, formatUnits } from "ethers/lib/utils"
 
 const toBN = (value) => BigNumber.from(value)
 const getBlockTimestamp = () => Math.floor((new Date()).getTime() / 1000.0)
 const toImgUrl = (url) => `${API_BASE_URI}/${url}`
 const toRawUrl = (itemId) => `${API_BASE_URI}/items/raw/${itemId}`
-const getTokenIdFromItemId = (itemId) => itemId.substring(43)
-const timestampToDate = (timestamp) => new Date(Number(timestamp + '000')).toLocaleString().replace(',', ' - ')
+const toTxUrl = (tx) => BLOCK_EXPLORER_URL + '/tx/' + tx
+const toAddressUrl = (address) => BLOCK_EXPLORER_URL + '/address/' + address
+const formatHexString = (hexString, start = 3, end = 3) => hexString.substring(0, start) + '...' + hexString.substring(hexString.length - end)
+const toTokenId = (itemId) => itemId.substring(43)
 const formatPrice = (price) => commify(Number(formatUnits(price, '18')))
+const timestampToDate = (timestamp) => {
+    const date = new Date(timestamp).toLocaleString(undefined, {
+        hour12: false,
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    }).replace(',', ' - ')
+    return date.substring(3, 6) + date.substring(0, 2) + date.substring(5) 
+}
 
 export {
     toBN,
     getBlockTimestamp,
     toImgUrl,
     toRawUrl,
-    getTokenIdFromItemId,
+    toTxUrl,
+    toAddressUrl,
+    formatHexString,
+    toTokenId,
     timestampToDate,
     formatPrice,
 }
