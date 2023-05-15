a2d79cc06dbbdc92a5bf50e1b7dd4b7726c4ebee
luc.letuan <phantoinang2001@gmail.com>
2023-05-16 01:17:31 +0700

    Add several collection API

--- src/api/collections.js
+++ src/api/collections.js
@@ -1,9 +1,25 @@
-import { axiosUploadFileInstance } from "./axiosInstance";
+import { axiosInstance, axiosUploadFileInstance } from "./axiosInstance";
 
 function createCollection(formData) {
   return axiosUploadFileInstance.post('/collections/create', formData)
 }
 
+function getCollection(collection_address) {
+  return axiosInstance.get('/collections', { params: { collection_address } })
+}
+
+function updateCollectionDescription(formBody) {
+  return axiosInstance.post('/collections/update-description', formBody)
+}
+
+function updateCollectionPicture(formData) {
+  return axiosUploadFileInstance.post('/collections/update-picture', formData)
+}
+
+
 export {
   createCollection,
+  getCollection,
+  updateCollectionDescription,
+  updateCollectionPicture
 }
