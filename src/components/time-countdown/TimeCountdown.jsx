aa84dc233177a03ff85e9eddb102bca7c1ff86f5
luc.letuan <phantoinang2001@gmail.com>
2023-05-16 23:06:29 +0700

    Update countdown component

--- src/components/time-countdown/TimeCountdown.jsx
+++ src/components/time-countdown/TimeCountdown.jsx
@@ -1,12 +1,26 @@
-import { useEffect, useState } from 'react'
-import { Button, Image, OverlayTrigger, Tooltip } from 'react-bootstrap'
-
+import { useEffect, useRef, useState } from 'react'
 
 function TimeCountdown({ timeRemaining, className }) {
   const [timeCountdown, setTimeCountdown] = useState(timeRemaining)
+  const timer = useRef(timeRemaining)
+  const lastTimeCountdown = useRef(timeRemaining)
+
   useEffect(() => {
-    if (timeCountdown > 0) setTimeout(() => setTimeCountdown(timeCountdown - 1), 1000)
+    if (timer.current !== timeRemaining) {
+      timer.current = timeRemaining
+      lastTimeCountdown.current = timeRemaining
+      setTimeCountdown(timeRemaining)
+    }
+  }, [timeRemaining])
+
+  useEffect(() => {
+    if (timeCountdown > 0 && (lastTimeCountdown.current === timeCountdown)) {
+      lastTimeCountdown.current = timeCountdown - 1
+      setTimeout(setTimeCountdown, 1000, timeCountdown - 1)
+    } 
   }, [timeCountdown])
+
+  if (timeCountdown < 0) return (<span className={className + ' text-danger'}>0d 0h 0m 0s</span>)
   return (
     <span className={className}>
       {Math.floor(timeCountdown / 86400)}d { }
